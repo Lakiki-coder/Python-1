4703869184e0212d4ad433b61caa82ac65209541
Lakiki-coder <omogoyesodiq074@gmail.com>
2025-07-16 15:50:18 +0100

    Add files via upload

---
+++ mile_to_kilometer.py
@@ -0,0 +1,9 @@
+"""Convert miles to kilometer"""
+
+miles = 175
+
+MILES_PER_KILOMETER = 1.609
+
+kilometer = miles * MILES_PER_KILOMETER
+
+print(kilometer)
---
+++ price_per_liter.py
@@ -0,0 +1,7 @@
+total_budget = int(input('Enter total budget'))
+
+price_per_liter = 855
+
+number_of_liter = total_budget / price_per_liter
+
+print(number_of_liter)
